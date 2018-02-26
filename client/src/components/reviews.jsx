e871c70843dae9be1bf96b4e8815adaacfc4d738
Melvin Estrada <melvinlouie.estrada@gmail.com>
2018-02-25 20:14:19 -0800

    render dummy data to webpage

--- client/src/components/reviews.jsx
+++ client/src/components/reviews.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import mockReviews from '../../dummy_data/mock_reviews.js';
 
 class Reviews extends React.Component {
   constructor(props) {
     super(props);
-    this.state({
-      reviews: 
-    });
+    this.state = {
+      reviews: mockReviews,
+      businessId: 200,
+      businessName: 'lorem',
+    };
   }
+
   render() {
     return (
-      <div>hello</div>
+      <div className="feed">
+        <div className="header">Recommended Reviews for {this.state.businessName}</div>
+      </div>
     );
   }
 }
