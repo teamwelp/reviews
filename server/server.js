cf3428120d9e180912a1e6d9325790c9af388b38
Melvin Estrada <melvinlouie.estrada@gmail.com>
2018-04-23 16:43:24 -0700

    add default port

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const app = require('./index.js');
 
-const url = process.env.URL;
+const url = process.env.URL | 9000;
 
 const server = app.listen(url, () => {
   console.log(`listening to port ${url}`, process.env.MONGOLAB_URI);
