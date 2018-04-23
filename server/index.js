4c86886d7dc2a99356d0af56041949987c6fe2de
Melvin Estrada <melvinlouie.estrada@gmail.com>
2018-04-23 15:54:11 -0700

    change port env variable

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/../public'));
 
-mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.PORT);
 
 app.use(express.static(path.join(__dirname, '/../public')));
 
